02cdb029070ac289a20d52adb8e9d47e04b16c40
Kelly Keating <kelz@Kellys-MacBook.local>
2017-04-12 16:45:35 +1200

    buttonPressed method created

--- calculator.js
+++ calculator.js
@@ -1,6 +1,31 @@
 
 var display = document.getElementById("display");
+var equation = [];
+var numberHolder = "";
+var sum = 0;
 
 function buttonPressed(button){
+  if (!isNan(button)||button == "."){
+    numberHolder += button;
+  } else if(button == "ac"){
+    equation = [];
+    numberHolder = "";
+    sum = 0;
+  }else if(button == "ce"){
+    numberHolder = "";
+  } else if(button == "="){
+    solve();
+  } else {
+    if(equation.length == 0 && numberHolder == ""){
+      numberHolder = sum;
+    }
+    equation[equation.length] = numberHolder;
+    equation[equation.length] = button;
+    numberHolder = "";
+    //sum = 0 ??
+  }
+}
+
+function solve(){
 
 }
