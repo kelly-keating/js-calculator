c9eb4a7fcc56da578cebf0dc5bc7b3ec1537ae10
Kelly Keating <kelz@Kellys-MacBook.local>
2017-04-12 18:02:35 +1200

    adding and solve working

--- calculator.js
+++ calculator.js
@@ -4,22 +4,22 @@ var equation = [];
 var numberHolder = "";
 var sum = 0;
 
+
 function buttonPressed(button){
   if (!isNaN(button)||button == "."){
     numberHolder += button;
   } else if(button == "ac"){
-    equation = [];
-    numberHolder = "";
-    sum = 0;
+    resetCalc();
   }else if(button == "ce"){
     numberHolder = "";
   } else if(button == "="){
+    equation[equation.length] = parseFloat(numberHolder);
     solve();
   } else {
     if(equation.length == 0 && numberHolder == ""){
       numberHolder = sum;
     }
-    equation[equation.length] = numberHolder;
+    equation[equation.length] = parseFloat(numberHolder);
     equation[equation.length] = button;
     numberHolder = "";
     //sum = 0 ??
@@ -27,9 +27,64 @@ function buttonPressed(button){
   updateDisplay();
 }
 
-function solve(){
-  updateDisplay();
+
+function resetCalc(){
+  equation = [];
+  numberHolder = "";
+  sum = 0;
 }
+
+
+function solve(){ // remem to accomodate for negatives
+  var total = equation[0];
+
+  for(var i = 1; i+1 < equation.length; i = i+2){
+    var symbol = equation[i];
+    var num = 0;
+
+    if(equation[i+1] == ""){
+      if(!isNaN(equation[i+3])){
+        num = 0 - equation[i+3];
+        i = i+2;
+      } else {
+        displayError();
+        return;
+      }
+    } else {
+      num = equation[i+1];
+    }
+
+    switch(symbol) {
+      case '+':
+        total += num;
+        break;
+      case '-':
+        total -= num;
+        break;
+      case '*':
+        total *= num;
+        break;
+      case '/':
+        total /= num;
+        break;
+      default:
+        break;
+    }
+  }
+
+  sum = total;
+  updateDisplay();
+  equation = [];
+  numberHolder = "";
+}
+
+
+function displayError(){
+  console.log("You did a boo boo");
+  equation = [];
+  numberHolder = "";
+}
+
 
 function updateDisplay(){
   var eq = equation.join();
