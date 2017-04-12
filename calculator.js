b85291baacd4315535021aaadf258ef8a9a89c90
Kelly Keating <kelz@Kellys-MacBook.local>
2017-04-13 00:21:51 +1200

    irrelevant neg check removed

--- calculator.js
+++ calculator.js
@@ -1,12 +1,11 @@
 
-var display = document.getElementById("display");
 var equation = [];
 var numberHolder = "";
 var sum = 0;
 
 
 function buttonPressed(button){
-  if (!isNaN(button)||button == "."){
+  if (!isNaN(button) || button == "."){
     numberHolder += button;
   } else if(button == "ac"){
     resetCalc();
@@ -41,9 +40,7 @@ function buttonPressed(button){
     equation[equation.length] = button;
     numberHolder = "";
   }
-
   updateDisplay();
-
 }
 
 
@@ -52,7 +49,8 @@ function solve(){ // remem to accomodate for negatives
   for(var i = 1; i+1 < equation.length; i = i+2){
     var symbol = equation[i];
     var num = 0;
-    if(isNaN(equation[i+1])){
+
+    /*if(isNaN(equation[i+1])){
       if(!isNaN(equation[i+3])){
         num = 0 - equation[i+3];
         i = i+2;
@@ -60,9 +58,9 @@ function solve(){ // remem to accomodate for negatives
         displayError();
         return;
       }
-    } else {
+    } else {*/
       num = equation[i+1];
-    }
+
     switch(symbol) {
       case '+':
         total += num;
